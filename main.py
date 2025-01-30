a486e81ba979a61f85691c7b2af63b7681af2bfd
AhmedMetaoua <ahmedmtawahg@gmail.com>
2025-01-30 19:15:18 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from db_manager import init_db
+from gui import lancer_interface
+
+if __name__ == "__main__":
+    # Initialisation de la base de données
+    init_db()
+
+    # Lancer l'interface utilisateur
+    lancer_interface()
+    
